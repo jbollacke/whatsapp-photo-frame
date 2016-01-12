2f16fcf8dda1122f442ac33d13e90578c7f0b896
Jan Bollacke <jbollacke@gmail.com>
2016-01-12 08:02:16 +0000

    Fix imports.

--- stack.py
+++ stack.py
@@ -1,14 +1,9 @@
 from yowsup.layers.auth import YowAuthenticationProtocolLayer
-from yowsup.layers.protocol_messages import YowMessagesProtocolLayer
-from yowsup.layers.protocol_receipts import YowReceiptProtocolLayer
-from yowsup.layers.protocol_acks import YowAckProtocolLayer
-from yowsup.layers.protocol_media import YowMediaProtocolLayer
 from yowsup.layers.network import YowNetworkLayer
 from yowsup.layers.coder import YowCoderLayer
 from yowsup.common import YowConstants
 from yowsup.layers import YowLayerEvent
 from yowsup.stacks import YowStack, YOWSUP_CORE_LAYERS, YOWSUP_PROTOCOL_LAYERS_FULL
-from yowsup import env
 from yowsup.layers.axolotl import YowAxolotlLayer
 from palayer import PictureArchiverLayer
 from config import CREDENTIALS

33a37e16d4b977154e73bdb04d0f6abada7128e7
Jan Bollacke <jbollack@de.hellmann.net>
2017-05-18 18:15:10 +0200

    Reconnect on disconnect

--- stack.py
+++ stack.py
@@ -1,16 +1,14 @@
 from yowsup.layers.network import YowNetworkLayer
-from yowsup.layers import YowLayerEvent
 from yowsup.stacks import YowStackBuilder
 from palayer import PictureArchiverLayer
 from config import CREDENTIALS
 
-if __name__==  "__main__":
-    stackbuilder = YowStackBuilder()
+stackbuilder = YowStackBuilder()
 
-    stack = stackbuilder.pushDefaultLayers(True).push(PictureArchiverLayer).build()
+stack = stackbuilder.pushDefaultLayers(True).push(PictureArchiverLayer).build()
 
-    stack.setCredentials(CREDENTIALS)
-    stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))
-
+stack.setCredentials(CREDENTIALS)
+while True:
+    stack.getLayerInterface(YowNetworkLayer).connect()
     stack.loop()
 

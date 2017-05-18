f02a11c7bcd5744335c02ef98b9413b2ebbce01a
Jan Bollacke <jbollack@de.hellmann.net>
2017-05-18 18:15:57 +0200

    Configure stack to autotrust new identities

--- stack.py
+++ stack.py
@@ -2,12 +2,14 @@ from yowsup.layers.network import YowNetworkLayer
 from yowsup.stacks import YowStackBuilder
 from palayer import PictureArchiverLayer
 from config import CREDENTIALS
+from yowsup.layers.axolotl.props import PROP_IDENTITY_AUTOTRUST
 
 stackbuilder = YowStackBuilder()
 
 stack = stackbuilder.pushDefaultLayers(True).push(PictureArchiverLayer).build()
 
 stack.setCredentials(CREDENTIALS)
+stack.setProp(PROP_IDENTITY_AUTOTRUST, True)
 while True:
     stack.getLayerInterface(YowNetworkLayer).connect()
     stack.loop()
